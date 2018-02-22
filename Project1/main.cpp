8a4abafa5580baa116c8f15a142a71ffe31a5c9b
MPrzekop <przekop.michal@gmail.com>
2018-02-22 20:01:22 +0000

    added gyro and accelerometer display

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,4 +1,23 @@
+#include "accGyrRead.h"
+#include <iostream>
+#include <unistd.h>
 int main(int argc,char **argv)
 {
+	accGyrRead *acc = new accGyrRead;
+	while(1){
+		std::system("clear");
+	printf("%f acc x \n",acc->getXAcc());
+	//usleep(300000);
+	printf("%f acc y\n" ,acc->getYAcc());
+	//usleep(300000);
+	printf("%f acc z\n",acc->getZAcc());
+	//usleep(300000);
+	printf("%f rot x\n",acc->getXRot());
+	//usleep(300000);
+	printf("%f rot y\n",acc->getYRot());
+	//usleep(300000);
+	printf("%f rot z\n",acc->getZRot());
+	usleep(160000);
+}
 	return 0;
 }
